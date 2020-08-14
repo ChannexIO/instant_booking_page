01188bb7a80aaaf86f440cdac3f313e3ad7cb70d
Vlad Strigul <v.strigul@channex.io>
2020-08-14 21:41:50 +0600

    CNX-1308 Add hotel info section title

--- src/components/hotel_info_section/hotel_info/hotel_info.js
+++ src/components/hotel_info_section/hotel_info/hotel_info.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
 
 import styles from "./hotel_info.module.css";
 
 export default function HotelInfo({ description }) {
+  const { t } = useTranslation();
+
   if (!description) {
     return null;
   }
 
   return (
     <div>
+      <div className={styles.hotelInfoTitle}>{t("hotel_page:hotel_info")}:</div>
       <pre className={styles.hotelDescription}>{description}</pre>
     </div>  
   );
