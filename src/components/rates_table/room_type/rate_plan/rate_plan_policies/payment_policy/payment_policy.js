b1ad463cc31d8786673706d56cc5f335bbaa09e8
Vlad Strigul <v.strigul@channex.io>
2020-10-28 17:47:02 +0600

    CNX-1585 Fix eslint errors

--- src/components/rates_table/room_type/rate_plan/rate_plan_policies/payment_policy/payment_policy.js
+++ src/components/rates_table/room_type/rate_plan/rate_plan_policies/payment_policy/payment_policy.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Alert from 'components/alert';
 
-export default function PaymentPolicy({ paymentPolicy }) {
+export default function PaymentPolicy() {
   return (
     <Alert
       text="NO PREPAYMENT NEEDED - pay at the property"
