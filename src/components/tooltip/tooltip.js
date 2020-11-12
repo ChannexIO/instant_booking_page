0fd95a91d6a46889e3b273154a9ea5232cd59f34
Vlad Strigul <v.strigul@channex.io>
2020-11-13 01:50:07 +0600

    CNX-1674 Show tooltip depending on available space

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -1,16 +1,34 @@
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Overlay } from 'react-bootstrap';
 
 import styles from './tooltip.module.css';
 
+const OVERLAY_POSITIONS = {
+  top: 'top',
+  bottom: 'bottom',
+};
+
 export default function PoliciesInfo({ className, children }) {
   const [isShown, setIsShown] = useState(false);
   const [target, setTarget] = useState(null);
+  const [overlayPlacement, setOverlayPlacement] = useState(OVERLAY_POSITIONS.top);
   const ref = useRef();
 
+  const getOpenDirection = useCallback(() => {
+    const inputCoords = ref.current.getBoundingClientRect();
+    const isTooltipCloserToTop = inputCoords.y < (window.innerHeight / 2);
+
+    const { top, bottom } = OVERLAY_POSITIONS;
+
+    return isTooltipCloserToTop ? bottom : top;
+  }, [ref]);
+
   const handleShow = (event) => {
+    const openDirection = getOpenDirection();
+
     setIsShown(!isShown);
     setTarget(event.target);
+    setOverlayPlacement(openDirection);
   };
 
   const handleHide = () => {
@@ -33,7 +51,7 @@ export default function PoliciesInfo({ className, children }) {
         show={isShown}
         target={target}
         rootCloseEvent="click"
-        placement="top"
+        placement={overlayPlacement}
         container={ref.current}
         onHide={handleHide}
       >
