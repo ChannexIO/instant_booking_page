9b0ab71d76b3ce07f67750dceded56c8ef24270a
Vlad Strigul <v.strigul@channex.io>
2020-11-12 19:23:03 +0600

    CNX-1674 Update rates policies section show actual info

--- src/components/rates_table/room_type/rate_plan/rate_plan_policies/payment_policy/payment_policy.js
+++ src/components/rates_table/room_type/rate_plan/rate_plan_policies/payment_policy/payment_policy.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import Alert from 'components/alert';
 
-export default function PaymentPolicy() {
+export default function PaymentPolicy({ cancellationPolicy }) {
+  const { t } = useTranslation();
+
+  if (!cancellationPolicy) {
+    return null;
+  }
+
+  const { guaranteePaymentPolicy, currency, guaranteePaymentAmount } = cancellationPolicy;
+
+  const currencyUnit = guaranteePaymentPolicy === 'percent_based' ? '%' : currency;
+  const policyText = t(`payment_policies:types:${guaranteePaymentPolicy}`)
+    .replace('{amount}', guaranteePaymentAmount)
+    .replace('{currency}', currencyUnit);
+
   return (
     <Alert
-      text="NO PREPAYMENT NEEDED - pay at the property"
+      text={policyText}
       variant="success"
     />
   );
--- src/components/rates_table/room_type/rate_plan/rate_plan_policies/rate_plan_policies.js
+++ src/components/rates_table/room_type/rate_plan/rate_plan_policies/rate_plan_policies.js
@@ -5,20 +5,24 @@ import Cell from 'components/layout/cell';
 import CancellationPolicy from './cancellation_policy';
 import MealPolicy from './meal_policy';
 import PaymentPolicy from './payment_policy';
-import PoliciesInfo from './policies_info';
 
+// TODO - update one day and make it work
+// import PoliciesInfo from './policies_info';
 import styles from './rate_plan_policies.module.css';
 
-export default function RatePlanPolicies({ ratePlan }) {
+export default function RatePlanPolicies({ ratePlan, checkinDate }) {
   return (
     <Cell className={styles.policyCell}>
       <div className={styles.policyContainer}>
         <div className={styles.mealContainer}>
           <MealPolicy mealPolicy={ratePlan.mealPolicy} />
-          <PoliciesInfo ratePlan={ratePlan} />
+          {/* <PoliciesInfo ratePlan={ratePlan} /> */}
         </div>
-        <CancellationPolicy cancellationPolicy={ratePlan.cancellationPolicy} />
-        <PaymentPolicy paymentPolicy={ratePlan.paymentPolicy} />
+        <CancellationPolicy
+          cancellationPolicy={ratePlan.cancellationPolicy}
+          checkinDate={checkinDate}
+        />
+        <PaymentPolicy cancellationPolicy={ratePlan.cancellationPolicy} />
       </div>
     </Cell>
   );
