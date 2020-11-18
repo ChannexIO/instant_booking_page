b1e4ecc2371942a0192e520354dc9294b1c24bbb
Vlad Strigul <v.strigul@channex.io>
2020-11-18 12:16:39 +0600

    CNX-1675 Update routing

---
+++ src/routing/routes.js
@@ -0,0 +1,12 @@
+const composeRoute = (route) => {
+  const path = process.env.REACT_APP_BASE_PATH;
+
+  return `${path}${route}`;
+}
+
+const routes = {
+  hotelPage: composeRoute("/:channelId"),
+  paymentPage: composeRoute("/:channelId/payment_page"),
+};
+
+export default routes;
