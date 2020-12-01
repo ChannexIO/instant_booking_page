aac252739a4bf0dc0dbb3303a48e18751527cfcb
Vlad Strigul <v.strigul@channex.io>
2020-12-01 15:34:21 +0600

    CNX-1769 Add address formatting at footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,8 +7,30 @@ import GetChannelAd from './get_channel_ad';
 
 import styles from './footer.module.css';
 
+const MAX_ADDRESS_LINE_SIZE = 2;
+
+const getFormattedAddress = (address) => {
+  if (!address) {
+    return null;
+  }
+
+  const addressChunks = address.split(', ');
+  const groupedAddressChunks = [];
+
+  while (addressChunks.length) {
+    const newChunk = addressChunks.splice(0, MAX_ADDRESS_LINE_SIZE);
+
+    groupedAddressChunks.push(newChunk.join(', '));
+    groupedAddressChunks.push(<br/>);
+  }
+
+  return groupedAddressChunks;
+};
+
 export default function Footer({ property }) {
   const { title, address, email, phone } = property;
+
+  const formattedAddress = getFormattedAddress(address);
 
   return (
     <div className={styles.footerWrapper}>
@@ -16,7 +38,7 @@ export default function Footer({ property }) {
         {title}
       </SectionTitle>
       <div className={styles.footer}>
-        {address && <Link to="#/" type="location">{address}</Link>}
+        {formattedAddress && <Link to="#/" type="location">{formattedAddress}</Link>}
         {email && <Link to={email} type="mail">{email}</Link>}
         {phone && <Link to={phone} type="phone">{phone}</Link>}
       </div>
