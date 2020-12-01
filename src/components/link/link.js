12272768d63750f4e4c1205c5077b03197f6cfe5
Vlad Strigul <v.strigul@channex.io>
2020-12-01 14:53:48 +0600

    CNX-1769 Fix footer elements stylng

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -10,37 +10,44 @@ const LINK_PROPS_BY_TYPE = {
   url: {
     icon: null,
     prefix: '',
+    interactive: true,
   },
   phone: {
     icon: PhoneIcon,
     prefix: 'tel:',
+    interactive: true,
   },
   mail: {
     icon: MailIcon,
     prefix: 'mailto:',
+    interactive: true,
   },
   default: {
     icon: null,
     prefix: '',
+    interactive: true,
   },
   location: {
     icon: LocationIcon,
     prefix: '',
+    interactive: false,
   },
 };
 
 export default function Link({ type = 'url', to = '', children }) {
   const { [type]: linkAttrs = LINK_PROPS_BY_TYPE.default } = LINK_PROPS_BY_TYPE;
-  const { icon, prefix } = linkAttrs;
+  const { icon, prefix, interactive } = linkAttrs;
 
   return (
     <span>
       <a
         className={styles.link}
         href={`${prefix}${to}`}
+        disabled={!interactive}
       >
         {icon && (
           <img
+            className={styles.icon}
             src={icon}
             alt={type}
           />
