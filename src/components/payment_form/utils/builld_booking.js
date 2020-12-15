74e656707188ccc10971d2882a3578ded374ed58
Vlad Strigul <v.strigul@channex.io>
2020-12-15 19:41:36 +0600

    Update booking structure to match new api

--- src/components/payment_form/utils/builld_booking.js
+++ src/components/payment_form/utils/builld_booking.js
@@ -1,42 +1,13 @@
-import moment from 'moment';
-
 import dateFormatter from 'utils/date_formatter';
 
 // TODO - probably should be removed after api fix
 const DEFAULT_CHECKIN_TIME = '12:00';
 
-const getOccupiedDatesList = (checkinDate, checkoutDate) => {
-  const isDatesValid = moment(checkinDate).isValid() && moment(checkoutDate).isValid();
-  const occupiedDatesList = [];
-
-  if (!isDatesValid) {
-    return occupiedDatesList;
-  }
-
-  for (
-    const currentDate = checkinDate.clone();
-    checkoutDate.diff(currentDate, 'days');
-    currentDate.add(1, 'days')
-  ) {
-    occupiedDatesList.push(dateFormatter.toApi(currentDate));
-  }
-
-  return occupiedDatesList;
-};
-
-const getBookedRoomDays = (dates, price, ratePlanCode) => {
-  return dates.map((date) => ({
-    date,
-    price,
-    ratePlanCode,
-  }));
-};
-
 const formatCardInfo = (cardInfo) => {
-  const { expirationMonth, expirationYear, ...rest } = cardInfo;
+  const { expirationMonth, expirationYear, serviceCode, ...rest } = cardInfo;
   const expirationDate = `${expirationMonth}/${expirationYear}`;
 
-  return { ...rest, expirationDate };
+  return { ...rest, expirationDate, cvv: serviceCode };
 };
 
 const buildBooking = (property, rooms, params, cardInfo, formData) => {
@@ -49,34 +20,25 @@ const buildBooking = (property, rooms, params, cardInfo, formData) => {
     ratesOccupancyPerRoom,
     checkinDate,
     checkoutDate,
-    adults,
-    children,
-    infants = 0, // TODO - remove when(if) added to IBE
     currency,
   } = params;
   const arrivalDate = dateFormatter.toApi(checkinDate);
   const departureDate = dateFormatter.toApi(checkoutDate);
-  const bookedDatesList = getOccupiedDatesList(checkinDate, checkoutDate);
   const guarantee = formatCardInfo(cardInfo);
 
   const bookedRooms = Object.keys(ratesOccupancyPerRoom).reduce((roomsList, roomTypeCode) => {
     const selectedRates = ratesOccupancyPerRoom[roomTypeCode];
     const roomProps = rooms.find((room) => roomTypeCode === room.id);
 
-    const bookedPerRoomId = Object.keys(selectedRates).reduce((acc, rateId) => {
-      const rateOccupancy = selectedRates[rateId];
-      const ratePlanProps = roomProps.ratePlans.find((rate) => rateId === rate.id);
-      const pricePerDate = Number(ratePlanProps.totalPrice) / ratePlanProps.lengthOfStay;
+    const bookedPerRoomId = Object.keys(selectedRates).reduce((acc, ratePlanCode) => {
+      const rateSelectedAmount = selectedRates[ratePlanCode];
+      const { occupancy } = roomProps.ratePlans.find((rate) => ratePlanCode === rate.id);
 
-      const bookedRoomsPerRate = new Array(rateOccupancy).fill(null)
+      const bookedRoomsPerRate = new Array(rateSelectedAmount).fill(null)
         .map(() => ({
           roomTypeCode,
-          occupancy: {
-            adults,
-            children,
-            infants,
-          },
-          days: getBookedRoomDays(bookedDatesList, pricePerDate, rateId),
+          ratePlanCode,
+          occupancy,
         }));
 
       return [...acc, ...bookedRoomsPerRate];
