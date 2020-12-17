6108f46adce92c163c0f7172299d8871827897ea
Vlad Strigul <v.strigul@channex.io>
2020-12-17 21:52:32 +0600

    CNX-1866 Fix child icon shown when no spaces

--- src/components/rates_table/room_type/rate_plan/rate_plan_occupancy/rate_plan_occupancy.js
+++ src/components/rates_table/room_type/rate_plan/rate_plan_occupancy/rate_plan_occupancy.js
@@ -20,8 +20,8 @@ const getAdditionalSpaces = (spaces, baseAmount) => {
 export default function RatePlanOccupancy({ occupancy, childrenOccupancy }) {
   const { adults, children } = occupancy;
   const additionalChildSpaces = getAdditionalSpaces(children, CHILD_BASE_NUMBER);
+  const isChildIconShown = children && childrenOccupancy;
   const additionalAdultsSpaces = getAdditionalSpaces(adults, ADULT_BASE_NUMBER);
-
   const adultsOccupancyIcon = adults === 2 ? IconGuestDouble : IconGuestSingle;
 
   return (
@@ -32,7 +32,7 @@ export default function RatePlanOccupancy({ occupancy, childrenOccupancy }) {
           additionalSpaces={additionalAdultsSpaces}
           icon={adultsOccupancyIcon}
         />
-        {Boolean(childrenOccupancy) && (
+        {Boolean(isChildIconShown) && (
           <OccupancySection
             type="children"
             additionalSpaces={additionalChildSpaces}
