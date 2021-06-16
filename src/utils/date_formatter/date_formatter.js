29d7221ad85c84673071cfcc69f15a7ed084290d
Vlad Strigul <vladstrigyl@yandex.ru>
2021-06-16 19:08:41 +0600

    Tests - add test for date formatter

--- src/utils/date_formatter/date_formatter.js
+++ src/utils/date_formatter/date_formatter.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-import { DATE_API_FORMAT, DATE_FORMAT } from "constants/formats";
+import { DATE_API_FORMAT, DATE_FORMAT, DATE_UI_FORMAT, DATE_UI_FULL_MONTH_FORMAT } from "constants/formats";
 
 const formatDate = (format) => (date) => {
   return date && moment(date).isValid() ? moment(date).format(format) : null;
@@ -9,4 +9,6 @@ const formatDate = (format) => (date) => {
 export default {
   toClient: formatDate(DATE_FORMAT),
   toApi: formatDate(DATE_API_FORMAT),
+  toUi: formatDate(DATE_UI_FORMAT),
+  toUiFullMonth: formatDate(DATE_UI_FULL_MONTH_FORMAT),
 };
