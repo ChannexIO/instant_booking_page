97f992102763bccf7df1f523bb2593f1d40e2327
Vlad Strigul <vladstrigyl@yandex.ru>
2021-07-01 21:06:39 +0600

    CNX-2846 Add ability to set language via url

--- src/components/inputs/locale_select/locale_select.js
+++ src/components/inputs/locale_select/locale_select.js
@@ -1,7 +1,11 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+import { useHistory } from "react-router-dom";
 
 import Select from "components/inputs/select";
+
+import getUrlParams from "utils/get_url_params";
+import setUrlParams from "utils/set_url_params";
 
 const AVAILABLE_LANGUAGES = [
   {
@@ -28,13 +32,26 @@ const AVAILABLE_LANGUAGES = [
 
 export default function LocaleSelect() {
   const { t, i18n } = useTranslation();
+  const history = useHistory();
 
   const handleLocaleChange = useCallback(
     (localeKey) => {
       i18n.changeLanguage(localeKey);
+
+      setUrlParams({ lang: localeKey }, history);
     },
-    [i18n],
+    [i18n, history],
   );
+
+  useEffect(function setSelectedLanguage() {
+    const { lang } = getUrlParams();
+
+    if (!lang) {
+      return;
+    }
+
+    i18n.changeLanguage(lang);
+  }, []);
 
   return (
     <Select
