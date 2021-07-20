99ab36ce1f968a2b736375243dffcb1d656836ab
Vlad Strigul <vladstrigyl@yandex.ru>
2021-07-20 12:51:26 +0600

    CNX-2938 Add browser language detection

---
+++ src/constants/supported_languages.js
@@ -0,0 +1,24 @@
+const supportedLanguages = [
+  {
+    key: "en",
+    value: "ENG",
+  },
+  {
+    key: "tr",
+    value: "TUR",
+  },
+  {
+    key: "es",
+    value: "ESP",
+  },
+  {
+    key: "pt",
+    value: "PRT",
+  },
+  {
+    key: "it",
+    value: "ITA",
+  },
+];
+
+export default supportedLanguages;
