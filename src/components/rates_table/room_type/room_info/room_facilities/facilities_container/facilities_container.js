06756e27bdb12206861fcdcb5c82279523aedc07
Andrew Yudin <pasarian@gmail.com>
2021-12-02 13:02:34 +0600

    CNX-3328 Add translations for Room Type Facilities

--- src/components/rates_table/room_type/room_info/room_facilities/facilities_container/facilities_container.js
+++ src/components/rates_table/room_type/room_info/room_facilities/facilities_container/facilities_container.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import Caption from "components/caption";
 
 const FACILITY_DIVIDER = " · ";
 
 export default function FacilitiesContainer({ facilities }) {
-  return <Caption variant="green">{facilities.join(FACILITY_DIVIDER)}</Caption>;
+  const { t } = useTranslation();
+  const translated_facilities = facilities.map(facility => {
+    return t(`facilities:${facility.toLowerCase()}`);
+  });
+
+  return <Caption variant="green">{translated_facilities.join(FACILITY_DIVIDER)}</Caption>;
 }
