4343a4b4cb338a9a7e85de1c88be236bb36cafab
Andrew Yudin <pasarian@gmail.com>
2022-06-16 14:15:48 +0600

    CNX-3495 Paid parking rule

--- src/components/hotel_additional_info_section/policies/parking_policies.js
+++ src/components/hotel_additional_info_section/policies/parking_policies.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import Currency from "components/currency";
+
 import InfoEntry from "./info_entry";
 
 const TRANSLATION_PATH = "hotel_page:hotel_policy:parking";
 
 export default function ParkingPolicies({ hotelPolicy }) {
   const { t } = useTranslation();
-  const { parkingIsPrivate, parkingReservation, parkingType } = hotelPolicy;
+  const { parkingIsPrivate, parkingReservation, parkingType, currency } = hotelPolicy;
+  const parkingCost = "30.00";
 
   const isParkingAvailable = parkingType !== "none";
   const parkingProperty = parkingIsPrivate ? "private" : "public";
+  const parkingPrice = parkingCost ? (
+    <Currency amount={parkingCost} currency={currency} />
+  ) : (
+    t(`${TRANSLATION_PATH}:cost_options:free`)
+  );
 
   return (
     <>
@@ -28,6 +36,7 @@ export default function ParkingPolicies({ hotelPolicy }) {
             title={t(`${TRANSLATION_PATH}:property_title`)}
             text={t(`${TRANSLATION_PATH}:property_options:${parkingProperty}`)}
           />
+          <InfoEntry title={t(`${TRANSLATION_PATH}:cost_title`)} text={parkingPrice} />
         </>
       )}
     </>
