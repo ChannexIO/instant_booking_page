6406fcaed92f4e71ff387bf3862d22125539cebf
Andrew Yudin <pasarian@gmail.com>
2022-08-24 17:09:39 +0600

    CNX-3797 Cutoff room type description at table

--- src/components/rates_table/room_type/room_info/description/description.js
+++ src/components/rates_table/room_type/room_info/description/description.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 import Caption from "components/caption";
 
-export default function Description({ description }) {
+import styles from "./description.module.css";
+
+export default function Description({ description,  handleModalToggle }) {
+  const { t } = useTranslation();
   if (!description) {
     return null;
+  }
+
+  const indices = [];
+  for(let i = 0; i < description.length; i++) {
+    if (description[i] === " ") indices.push(i);
+  }
+  const cutoff = indices.filter(x => x > 200)[0];
+
+  if (cutoff) {
+    return (
+      <Caption>
+        {description.slice(0, cutoff)}...
+        <br />
+        <button className={styles.button} onClick={handleModalToggle}>{t("hotel_page:read_more")}</button>
+      </Caption>
+    );
   }
 
   return <Caption>{description}</Caption>;
