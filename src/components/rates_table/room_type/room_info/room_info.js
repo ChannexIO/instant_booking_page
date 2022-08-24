f46178a99a0df93cac4eca4dff2b20ce7366a7f8
Andrew Yudin <pasarian@gmail.com>
2022-08-24 17:09:39 +0600

    CNX-3797 Cutoff room type description at table

--- src/components/rates_table/room_type/room_info/room_info.js
+++ src/components/rates_table/room_type/room_info/room_info.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import Cell from "components/layout/cell";
 
@@ -7,19 +7,26 @@ import Description from "./description";
 import RoomAvailability from "./room_availability";
 import RoomFacilities from "./room_facilities";
 import RoomTitle from "./room_title";
+import RoomDetailsModal from "./room_details_modal";
 
 import styles from "./room_info.module.css";
 
 export default function RoomInfo({ roomType }) {
   const { availability, bedOptions, facilities, description } = roomType;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalToggle = useCallback(() => {
+    setIsModalOpen(!isModalOpen);
+  }, [isModalOpen, setIsModalOpen]);
 
   return (
     <Cell className={styles.roomContainer}>
-      <RoomTitle room={roomType} />
-      <Description description={description} />
+      <RoomTitle room={roomType} handleModalToggle={handleModalToggle} />
+      <Description description={description} handleModalToggle={handleModalToggle} />
       <RoomAvailability availability={availability} />
       <BedOptions bedOptions={bedOptions} />
       <RoomFacilities facilities={facilities} />
+      <RoomDetailsModal room={roomType} show={isModalOpen} onHide={handleModalToggle} />
     </Cell>
   );
 }
--- src/components/rates_table/room_type/room_info/room_title/room_title.js
+++ src/components/rates_table/room_type/room_info/room_title/room_title.js
@@ -1,23 +1,16 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { Button } from "react-bootstrap";
-
-import RoomDetailsModal from "./room_details_modal";
 
 import styles from "./room_title.module.css";
 
-export default function RoomTitle({ room }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+export default function RoomTitle({ room, handleModalToggle }) {
 
-  const handleModalToggle = useCallback(() => {
-    setIsModalOpen(!isModalOpen);
-  }, [isModalOpen, setIsModalOpen]);
 
   return (
     <>
       <Button variant="link" className={styles.title} onClick={handleModalToggle}>
         <strong>{room.title}</strong>
       </Button>
-      <RoomDetailsModal room={room} show={isModalOpen} onHide={handleModalToggle} />
     </>
   );
 }
